49a93f18d4bf849b468df4b8ea6a6eb7991f77d0
Gonçalo <gapfranco@gmail.com>
2021-01-13 13:48:55 -0300

    feat: :sparkles: translation & styles for email messages

--- lib/advance/accounts/user_notifier.ex
+++ lib/advance/accounts/user_notifier.ex
@@ -1,6 +1,7 @@
 defmodule Advance.Accounts.UserNotifier do
   import Bamboo.Email
   alias Advance.Mailer
+  import AdvanceWeb.Gettext
 
   @from_address "app@m2isrv.com"
 
@@ -22,87 +23,207 @@ defmodule Advance.Accounts.UserNotifier do
   Deliver instructions to confirm account.
   """
   def deliver_confirmation_instructions(user, url) do
-    text_body = """
+    locale = Gettext.get_locale(AdvanceWeb.Gettext)
 
-    ==============================
+    text_body =
+      cond do
+        locale == "en" ->
+          """
 
-    Hi #{user.email},
+          ==============================
 
-    You can confirm your account by visiting the url below:
+          Account confirmation
 
-    #{url}
+            #{user.name},
 
-    If you didn't create an account with us, please ignore this.
+          You can confirm your account by visiting the url below:
 
-    ==============================
-    """
+          #{url}
 
-    html_body = """
-    Hi #{user.email},<br/></br/>
-    You can confirm your account by visiting the url below:<br/></br/>
-    <a href="#{url}" target="_blank">#{url}</a><br/></br/>
-    If you didn't create an account with us, please ignore this.
-    """
+          If you didn't create an account with us, please ignore this.
 
-    deliver(user.email, "Please confirm your account", text_body, html_body)
+          ==============================
+          """
+
+        true ->
+          """
+
+          ==============================
+
+          Confirmação da conta
+
+            #{user.name},
+
+          Você pode confirmar sua conta visitando o endereço abaixo:
+
+          #{url}
+
+          Se não criou uma conta conosco, por favor ignore esse e-mail.
+
+          ==============================
+          """
+      end
+
+    html_body =
+      cond do
+        locale == "en" ->
+          """
+          <h2>Account confirmation</h2>
+          <p>#{user.name},</p><br/></br/>
+          Confirm your account by visiting the url below:<br/></br/>
+          <a href="#{url}" target="_blank">#{url}</a><br/></br/>
+          If you didn't create an account with us, please ignore this.
+          """
+
+        true ->
+          """
+          <h2>Confirmação da conta</h2>
+          <p>#{user.name},</p><br/></br/>
+          Você pode confirmar sua conta visitando o endereço abaixo:<br/></br/>
+          <a href="#{url}" target="_blank">#{url}</a><br/></br/>
+          Se não criou uma conta conosco, por favor ignore esse e-mail.
+          """
+      end
+
+    deliver(user.email, gettext("Please confirm your account"), text_body, html_body)
   end
 
   @doc """
   Deliver instructions to reset password account.
   """
   def deliver_reset_password_instructions(user, url) do
-    text_body = """
+    locale = Gettext.get_locale(AdvanceWeb.Gettext)
 
-    ==============================
+    text_body =
+      cond do
+        locale == "en" ->
+          """
 
-    Hi #{user.email},
+          ==============================
 
-    You can reset your password by visiting the url below:
+          Reset password
 
-    #{url}
+          #{user.name},
 
-    If you didn't request this change, please ignore this.
+          You can reset your password by visiting the url below:
 
-    ==============================
-    """
+          #{url}
 
-    html_body = """
-    Hi #{user.email},<br/></br/>
-    You can reset your password by visiting the url below:<br/></br/>
-    <a href="#{url}" target="_blank">#{url}</a><br/></br/>
-    If you didn't request this change, please ignore this.
-    """
+          If you didn't request this change, please ignore this.
 
-    deliver(user.email, "Please confirm your account", text_body, html_body)
+          ==============================
+          """
+
+        true ->
+          """
+
+          ==============================
+
+          Recriar senha
+
+          #{user.name},
+
+          Você pode recriar sua senha acessando o endereço abaixo:
+
+          #{url}
+
+          Se você não solicitou a mudança, por favor ignore isso.
+
+          ==============================
+          """
+      end
+
+    html_body =
+      cond do
+        locale == "en" ->
+          """
+          <h2>Reset password</h2>
+          <p>#{user.name},</p><br/></br/>
+          You can reset your password by visiting the url below:<br/></br/>
+          <a href="#{url}" target="_blank">#{url}</a><br/></br/>
+          If you didn't request this change, please ignore this.
+          """
+
+        true ->
+          """
+          <h2>Recriar senha</h2>
+          <p>#{user.name},</p><br/></br/>
+          Você pode recriar as sua senha acessando o endereço abaixo:<br/></br/>
+          <a href="#{url}" target="_blank">#{url}</a><br/></br/>
+          Se você não solicitou a mudança, por favor ignore isso.
+          """
+      end
+
+    deliver(user.email, gettext("Password reset instructions"), text_body, html_body)
   end
 
   @doc """
   Deliver instructions to update your e-mail.
   """
   def deliver_update_email_instructions(user, url) do
-    text_body = """
+    locale = Gettext.get_locale(AdvanceWeb.Gettext)
 
-    ==============================
+    text_body =
+      cond do
+        locale == "en" ->
+          """
 
-    Hi #{user.email},
+          ==============================
 
-    You can change your e-mail by visiting the url below:
+          Change email
 
-    #{url}
+          Hi #{user.name},
 
-    If you didn't request this change, please ignore this.
+          You can change your e-mail by visiting the url below:
 
-    ==============================
-    """
+          #{url}
 
-    html_body = """
-    Hi #{user.email},<br/></br/>
-    You can change your e-mail by visiting the url below:<br/></br/>
-    <a href="#{url}" target="_blank">#{url}</a><br/></br/>
-    If you didn't request this change, please ignore this.
-    """
+          If you didn't request this change, please ignore this.
 
-    deliver(user.email, "Please confirm your account", text_body, html_body)
+          ==============================
+          """
+
+        true ->
+          """
+
+          ==============================
+
+          Mudança de email
+
+          #{user.name},
+
+          Você pode mudar seu email acessando o endereço abaixo:
+
+          #{url}
+
+          Se você não solicitou a mudança, por favor ignore isso.
+
+          ==============================
+          """
+      end
+
+    html_body =
+      cond do
+        locale == "en" ->
+          """
+          <h2>Change email</h2>
+          <p>Hi #{user.name},</p><br/></br/>
+          You can change your e-mail by visiting the url below:<br/></br/>
+          <a href="#{url}" target="_blank">#{url}</a><br/></br/>
+          If you didn't request this change, please ignore this.
+          """
+
+        true ->
+          """
+          <h2>Mudança de email</h2>
+          <p>#{user.name},</p><br/></br/>
+          Você pode mudar seu email acessando o endereço abaixo:<br/></br/>
+          <a href="#{url}" target="_blank">#{url}</a><br/></br/>
+          Se você não solicitou a mudança, por favor ignore isso.
+          """
+      end
+
+    deliver(user.email, gettext("Email change instructions"), text_body, html_body)
   end
 end
 
